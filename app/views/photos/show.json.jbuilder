cab8c8a2878b445f9b9e70344577244eb8b186ca
Michael Osiecki <michael@nguteam.com>
2013-09-30 15:56:11 +1000

    Added photo scaffold title description

---
+++ app/views/photos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @photo, :title, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930054555) do
+ActiveRecord::Schema.define(version: 20130930055328) do
+
+  create_table "photos", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
